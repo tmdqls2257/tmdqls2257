f96e67e9481da6ace3d9744a9ff5348d8daeecb4
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-05-25 22:25:02 +0900

    Update object.js

--- object.js
+++ object.js
@@ -36,3 +36,49 @@ function printValue(obj, key){
 }
 printValue(ellie, 'name');
 printValue(ellie, 'age');
+
+// 3. Property value shorthand
+const person1 = {name: 'bob', age:2};
+const person2 = {name: 'dave', age:4};
+const person3 = {name: 'steve', age:3};
+const person4 = new Person('ellie', 30);
+
+// 4. Constuctor function
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+}
+
+// 5. in operator: property existence check (key in obj)
+console.log('name' in ellie);
+console.log('age' in ellie);
+console.log('random' in ellie);
+console.log(ellie.random);
+
+// 6. for..in vs for..of
+// for (key in obj)
+console.clear();
+for(key in ellie){
+  console.log(key);
+}
+
+// for (value of iterable)
+const array = [1, 2, 3, 4, 5];
+for(value of array){
+  console.log(value);
+}
+
+// 7. Fun cloning
+// Object.assign(dest, [obj1 ,obj2, obj3...])
+const user = { name: 'ellie', age:'20'};
+const user2 = user;
+user2.name = 'coder';
+console.log(user);
+
+// old way
+const user3 = {};
+for (key in user){
+  user3[key] = user[key];
+}
+console.clear();
+console.log(user3);
